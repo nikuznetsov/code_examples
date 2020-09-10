45f11b27900223d348a39e3d41d75e780a0b4ea1
Nikita Kuznetsov <nikuznetsoff@gmail.com>
2020-09-10 15:32:09 +0300

    Update bisection_method.py

--- bisection_method.py
+++ bisection_method.py
@@ -7,14 +7,17 @@ from time import time
 import sys
 
 def toFixed(numObj):
-    digits=5
-    return f"{numObj:.{digits}f}"
+	# fixed number of digits
+	digits=5
+	return f"{numObj:.{digits}f}"
 
 def f(x):
+	# introduction of function
 	r=eval(function)
 	return r	
 
 def bisection(a,b,err):
+	# implementation of method
 	c=(a+b)/2.0
 	while abs(b-a)>err:
 		if f(a)*f(c)<0:
@@ -27,6 +30,7 @@ def bisection(a,b,err):
 	print('The function equals with the root = ',toFixed(f(c)))
 	
 def searching(a,b):
+	# searching of root interval
 	a1=a
 	b1=b
 	while a1<b1:
